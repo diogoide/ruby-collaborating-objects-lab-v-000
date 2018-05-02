e9e51905e048d53c5a5ce7f7c7c42b8d03667cee
Diogo Ide <diogoide@gmail.com>
2018-05-02 23:36:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
    attr_accessor :name, :artist
- 
+
    def initialize(name)
      @name = name
    end
- 
+
    def self.new_by_filename(filename)
      artist_name, song_name, extra = filename.split(" - ")
      song = self.new(song_name)
