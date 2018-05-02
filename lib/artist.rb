b5e71d510d218194c8dfb432384c851b7ba383be
Diogo Ide <diogoide@gmail.com>
2018-05-02 23:35:50 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,49 +1,37 @@
 class Artist
-  attr_accessor :name, :songs
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def songs
-    @songs
-  end
-
-  def self.all
-    @@all
-  end
-  
-  def save
-    @@all << self
-  end
-
-  def add_song(song)
-    @songs << song
-    #song.artist = self
-  end
-
-  def self.find(name)
-    self.all.find {|artist| artist.name == name}
-  end
-
-  def self.create(name)
-    self.new(name).tap {|artist| artist.save}
-    #@@all << self
-  end
-
-  def self.find_or_create_by_name(name)
-    artist = @@all.find {|artist| artist.name == name}
-    if artist == nil
-      artist = Artist.new(name)
-      artist.save
+   attr_accessor :name
+ 
+   @@all = []
+ 
+   def initialize(name)
+     @name = name
+     @songs= []
+   end
+ 
+   def add_song(song)
+     @songs << song
+     song.artist = self
+     song
+   end
+ 
+   def songs
+     @songs
+   end
+ 
+   def save
+     @@all << self
+     self
+   end
+ 
+   def self.all
+     @@all
+   end
+ 
+   def self.find_or_create_by_name(name)
+     self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+   end
+ 
+   def print_songs
+     self.songs.each {|song| puts song.name} 
     end
-      @@all.last
-    end
-
-  def print_songs
-    songs.each {|song| puts song.name}
-  end
-
-end
+ end
